baa62463efcb2bc12dc4045259617a37688117d6
it1527 <32055618+it1527@users.noreply.github.com>
2019-01-10 18:26:35 +0100

    Add files via upload

---
+++ pokusy/display/pokus_v_IDE.ino
@@ -0,0 +1,39 @@
+
+//knihovna LCD 
+#include <LiquidCrystal_I2C.h>
+// Construct an LCD object and pass it the 
+// I2C address, width (in characters) and
+// height (in characters). Depending on the
+// Actual device, the IC2 address may change.
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+
+void setup(){
+  Serial.begin(115200);
+  Serial.write("kokot");
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+
+  // The begin call takes the width and height. This
+  // Should match the number provided to the constructor.
+  lcd.begin(16,2);
+  lcd.init();
+
+  // Turn on the backlight.
+  lcd.backlight();
+
+  // Move the cursor characters to the right and
+  // zero characters down (line 1).
+  lcd.setCursor(5, 0);
+
+  // Print HELLO to the screen, starting at 5,0.
+  lcd.print("katarina,");
+
+  // Move the cursor to the next line and print
+  // WORLD.
+  lcd.setCursor(5, 1);      
+  lcd.print("mi zadek");
+}
+
+ void loop(void) {
+}
